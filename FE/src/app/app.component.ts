228d7d4e1316228645f56e3ed9399bf16be47f40
AlonsoFuentesGTZ <jesus.fuentescondega@ucr.ac.cr>
2024-09-14 15:43:21 -0600

    Se agregó un boton para hacer un PopUp

--- FE/src/app/app.component.ts
+++ FE/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {UserCardComponent} from './pages/userCard/userCard.component';
-import { RoomComponent } from "./pages/room/room.component";
+import { UserCardComponent } from './pages/userCard/userCard.component';
+import { RoomComponent } from "./pages/Room/room.component";
 import { JsonPipe } from '@angular/common';
 import { CreateRoomComponent } from './pages/createRoom/createRoom.component';
-import { ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
+import { getRoomComponent } from "./pages/getRoom/getRoom.component";
+
 
 
 
@@ -16,10 +18,10 @@ import { MatCardModule } from '@angular/material/card';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RoomComponent,
-     JsonPipe,ReactiveFormsModule,
-     MatInputModule,
-     MatButtonModule,
-     MatFormFieldModule,MatCardModule,CreateRoomComponent],
+    JsonPipe, ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule, MatCardModule, CreateRoomComponent, getRoomComponent],
   templateUrl: './app.component.html',
 })
 
---
+++ FE/src/app/pages/getRoom/getRoom.component.ts
@@ -0,0 +1,36 @@
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+/**
+ * @title Dialog with header, scrollable content and actions
+ */
+@Component({
+  selector: 'getRoom',
+  templateUrl: './getRoom.component.html',
+  styleUrl: './getRoom.component.css',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class getRoomComponent {
+  readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    const dialogRef = this.dialog.open(getRoomPopUpComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+
+@Component({
+  selector: 'getRoomPopUp',
+  templateUrl: './getRoomPopUp.component.html',
+  styleUrl: './getRoom.component.css',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class getRoomPopUpComponent {}
