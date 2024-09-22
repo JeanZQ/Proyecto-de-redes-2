d26874d7481ac61d461ba3b81b288516980150a6
Raúl Miranda <raulmirandavargas82@gmail.com>
2024-09-21 18:26:39 -0600

    Creación componente welcome

--- FE/src/app/app.routes.ts
+++ FE/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { CreateRoomComponent } from './pages/createRoom/createRoom.component';
 import { RoomComponent } from './pages/Room/room.component';
 import { LobbyComponent } from './pages/lobby/lobby.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 export const routes: Routes = [
-    {path:'',component:RoomComponent},
-    {path:'createRoom',component:CreateRoomComponent},
+    {path:'',component:WelcomeComponent},
+    // {path:'createRoom',component:CreateRoomComponent},
     {path:'lobby',component:LobbyComponent}
 ];
---
+++ FE/src/app/welcome/welcome.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RoomComponent } from '../pages/Room/room.component';
+import { CreateRoomComponent } from '../pages/createRoom/createRoom.component';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [RoomComponent,CreateRoomComponent],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+
+}
